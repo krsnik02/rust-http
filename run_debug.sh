2becb6748f663d3cbe7201e523e6ea1b091f9b54
Garrett Hartshaw <ghartshaw@gmail.com>
2015-10-09 17:38:52 -0400

    Added logging

---
+++ run_debug.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo RUST_LOG=rust_http2=debug cargo run
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[macro_use] extern crate log;
+extern crate env_logger;
+
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init().unwrap();
+
+    match TcpListener::bind("localhost:80") {
+        Err(err) => {
+            // Unable to bind socket
+            error!("Error: {}", err)
+        },
+        Ok(listener) => {
+            // Successfully bound socket
+            info!("Success: {:?}", listener)
+        }
+    }
 }
