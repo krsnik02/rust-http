879bd24d4e892da317cff1fdcece9af0765b87a9
Garrett Hartshaw <ghartshaw@gmail.com>
2015-10-12 03:56:40 -0400

    Use io::Error instead of custom error type

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,9 @@
 extern crate mio;
 
-use std::io;
-use std::result;
+use std::io::Result;
 use std::net::{SocketAddr, ToSocketAddrs};
 use mio::{EventLoop, Handler, Token};
 use mio::tcp::{TcpListener, TcpStream};
-
-#[derive(Debug)]
-pub struct HttpError;
-
-pub type Result<T> = result::Result<T, HttpError>;
-
 
 pub struct HttpConnection {
     tcp_stream: TcpStream,
@@ -19,12 +12,10 @@ pub struct HttpConnection {
 impl HttpConnection {
     pub fn local_addr(&self) -> Result<SocketAddr> {
         self.tcp_stream.local_addr()
-            .map_err(|_| HttpError)
     }
 
     pub fn peer_addr(&self) -> Result<SocketAddr> {
         self.tcp_stream.peer_addr()
-            .map_err(|_| HttpError)
     }
 }
 
@@ -46,8 +37,7 @@ impl HttpServer {
 
     /// Registers itself on the given `EventLoop`.
     pub fn register_self<H : Handler>(
-        &self, event_loop: &mut EventLoop<H>, token: Token)
-        -> io::Result<()> {
+        &self, event_loop: &mut EventLoop<H>, token: Token) -> Result<()> {
         event_loop.register(&self.tcp_listener, token)
     }
 }
